82879a3c8857a5e8e508208eb693f4b82ec5618a
Fabricio-Prudente-Ferreira <fabricio.ferreira@sptech.school>
2024-05-18 19:08:53 -0300

    Conexão com Banco de Dados - Cadastro & Login

---
+++ web-data-viz/public/js/sessao.js
@@ -0,0 +1,33 @@
+// sessão
+function validarSessao() {
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    var b_usuario = document.getElementById("b_usuario");
+
+    if (email != null && nome != null) {
+        b_usuario.innerHTML = nome;
+    } else {
+        window.location = "../login.html";
+    }
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "../login.html";
+}
+
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+function finalizarSessao(texto) {
+    // var divAguardar = document.getElementById("div_aguardar");
+    // divAguardar.style.display = "none";
+
+    var divErro = document.getElementById("div_erro");
+    if (texto) divErro.innerHTML = texto;
+}
+
---
+++ web-data-viz/src/models/usuarioModel.js
@@ -0,0 +1,28 @@
+var database = require("../database/config")
+
+function autenticar(login, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", login, senha)
+    var instrucaoSql = `
+        SELECT idUsuario, nickname, nome, email FROM Usuario WHERE (email = '${login}' OR nickname = '${login}') AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, nick, email, dtNasc, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO Usuario (nome, nickname, email, dtNasc, senha) VALUES ('${nome}', '${nick}', '${email}', '${dtNasc}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
