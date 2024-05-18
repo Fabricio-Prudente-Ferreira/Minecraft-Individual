7ea33f0d968cf72ddbd4a015b77e0b9785857153
Fabricio-Prudente-Ferreira <fabricio.ferreira@sptech.school>
2024-05-18 19:40:34 -0300

    Banco de Dados - Conexão com a tela "Perfil"

---
+++ site/public/js/sessao.js
@@ -0,0 +1,58 @@
+// sessão
+function validarSessao() {
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nick = sessionStorage.NICK_USUARIO;
+
+    var nick_usuario = document.getElementById("nick_usuario");
+
+    if (email != null && nick != null) {
+        nick_usuario.innerHTML = nick;
+    } else {
+        window.location = "../login.html";
+    }
+}
+
+function load_user_data(){
+    var id = `#${sessionStorage.ID_USUARIO}`;
+    var name = sessionStorage.NOME_USUARIO;
+    var nick = sessionStorage.NICK_USUARIO;
+    var email = sessionStorage.EMAIL_USUARIO;
+    var birthdate = sessionStorage.DTNASC_USUARIO.split('T')[0];
+    var passWd = sessionStorage.SENHA_USUARIO;
+    var data_array = [id, name, nick, email, birthdate, passWd];
+
+    var elements_array = [
+        document.getElementById('inp_id'),
+        document.getElementById('inp_nome'),
+        document.getElementById('inp_nick'),
+        document.getElementById('inp_email'),
+        document.getElementById('inp_dtNasc'),
+        document.getElementById('inp_senha')
+    ];
+
+    console.log(data_array);
+
+    for(var index = 0; index < data_array.length; index++){
+        elements_array[index].value = data_array[index];
+    }
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "../login.html";
+}
+
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+function finalizarSessao(texto) {
+    // var divAguardar = document.getElementById("div_aguardar");
+    // divAguardar.style.display = "none";
+
+    var divErro = document.getElementById("div_erro");
+    if (texto) divErro.innerHTML = texto;
+}
+
